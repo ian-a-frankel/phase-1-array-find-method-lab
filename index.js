2a530c386aa9a26ab854d7f397be9057cb127d20
Ian Frankel <ian.frankel@gmail.com>
2023-10-06 11:32:26 -0400

    find first win

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function superbowlWin(superbowls) {
+    let winsArray = []
+    let hasWon = false
+    superbowls.forEach(theyWonBoolean)
+    winsArray.forEach(a =>{
+        hasWon = hasWon || a})
+    
+    console.log(hasWon)
+    if (hasWon) {
+        const wonYear = superbowls.find((annum) => annum.result === 'W')
+        console.log(wonYear)
+        return wonYear.year
+    } else {
+        return undefined
+    }
+    function theyWonBoolean(superbowl) {
+        winsArray.push(superbowl.result === 'W')
+    }
+}
